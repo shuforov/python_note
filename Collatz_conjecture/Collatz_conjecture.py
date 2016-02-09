9f458d66cff5c04513bfa60218c8ee68a72792a0
shuforov <shuforov@gmail.com>
2016-02-09 10:23:36 +0200

    0.1

--- Collatz_conjecture/Collatz_conjecture.py
+++ Collatz_conjecture/Collatz_conjecture.py
@@ -2,11 +2,15 @@
 
 def collatz(num_coll):
 	z = num_coll
+	num_cycles = 0
 	while z > 1:
 		if (z % 2) == 0:
 			z = z / 2
+			num_cycles += 1
 		elif (z % 2) == 1:
 			z = (z * 3) + 1
+			num_cycles += 1
 		print z
-
-print collatz(142523423423423424663534752342352352352352)
+	print"Number Of Cycles -",num_cycles
+	return ""
+print collatz(32)
