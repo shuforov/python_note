b440ca97c1142fa9975d439187c52394b6317d07
shuforov <shuforov@gmail.com>
2016-02-09 10:16:25 +0200

    0.0

--- Collatz_conjecture/Collatz_conjecture.py
+++ Collatz_conjecture/Collatz_conjecture.py
@@ -1,1 +1,12 @@
 #Collatz_conjecture https://en.wikipedia.org/wiki/Collatz_conjecture
+
+def collatz(num_coll):
+	z = num_coll
+	while z > 1:
+		if (z % 2) == 0:
+			z = z / 2
+		elif (z % 2) == 1:
+			z = (z * 3) + 1
+		print z
+
+print collatz(142523423423423424663534752342352352352352)
